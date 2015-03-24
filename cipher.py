39845e2c620852067ffc315909d4141c9fbe0d07
davidmshou <david.m.shou@gmail.com>
2015-03-23 20:12:53 -0700

    Cool cipher program from class.

---
+++ cipher.py
@@ -0,0 +1,23 @@
+import random
+
+alphabet = "abcdefghijklmnopqrstuvwxyz "
+
+alphabet_list = [alphabet[i] for i in range(len(alphabet))]
+#alphabet_list = list(alphabet)
+
+print(alphabet_list)
+
+message_text = "gvxpuogvxogvxoyapt"
+
+cipher_list = ['z', 'k', 'q', 'p', 's', ' ', 'd', 'g', 'a', 'e', 'y', 'h', 'f', 'n', 'v', 'm', 'b', 'l', 't', 'r', 'c', 'i', 'x', 'w', 'u', 'j', 'o']
+
+#random.shuffle(cipher_list)
+
+cipher_text = ""
+
+for x in message_text:
+    index = cipher_list.index(x)
+    cipher_char = alphabet_list[index]
+    cipher_text += cipher_char
+
+print cipher_text
